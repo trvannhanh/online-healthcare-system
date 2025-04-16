38d02da10162e10d73f69a63eb12d171916bfd17
Nhanh <trvannhanh@gmail.com>
2025-04-16 16:40:23 +0700

    Spring Security & UserRepo

---
+++ HealCareApp/src/main/java/com/can/configs/SpringSecurityConfigs.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.can.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+/**
+ *
+ * @author Giidavibe
+ */
+@Configuration
+@EnableWebSecurity
+@EnableTransactionManagement
+@ComponentScan(basePackages = {
+    "com.can.controllers",
+    "com.can.repositories",
+    "com.can.services"
+})// chỉ định những nơi sử dụng annotaion
+public class SpringSecurityConfigs {
+
+    @Autowired
+    private UserDetailsService userDetailService;
+
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public HandlerMappingIntrospector mvcHandlerMappingIntrospector() {
+        return new HandlerMappingIntrospector();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+            Exception {
+        http.csrf(c -> c.disable()).authorizeHttpRequests(requests -> requests
+                .requestMatchers("/", "/home").authenticated()
+                .requestMatchers("/appointments/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+        )
+                .formLogin(form -> form.loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?error=true").permitAll())
+                .logout(logout -> logout.logoutSuccessUrl("/login").permitAll());
+        return http.build();
+    }
+}
