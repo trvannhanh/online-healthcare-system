d64413bf837a684732ad5f70004c346b9b100218
Lychee1811 <140429581+Lychee1811@users.noreply.github.com>
2025-05-13 13:14:09 +0700

    fix

--- HealCareApp/src/main/java/com/can/pojo/Patient.java
+++ HealCareApp/src/main/java/com/can/pojo/Patient.java
@@ -11,6 +11,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import java.util.Date;
 
 
